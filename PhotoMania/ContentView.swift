58c55147fd334e309101c76218e8157c8c7722c4
Marc <marcjacques@me.com>
2022-01-28 22:37:49 -0500

    Initial Commit

---
+++ PhotoMania/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoMania
+//
+//  Created by Marc Jacques on 1/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoMania/PhotoManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoManiaApp.swift
+//  PhotoMania
+//
+//  Created by Marc Jacques on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
