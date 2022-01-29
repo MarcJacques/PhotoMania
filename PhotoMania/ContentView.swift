a87ce0d90d98f2ecee7e68a2433004719d01def7
Marc <marcjacques@me.com>
2022-01-28 22:46:18 -0500

    Add basic UI to content view

--- PhotoMania/ContentView.swift
+++ PhotoMania/ContentView.swift
@@ -6,11 +6,36 @@
 //
 
 import SwiftUI
+// 
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                
+                Spacer()
+                
+                Image(systemName: "photo")
+                    .resizable()
+                    .foregroundColor(Color.pink)
+                    .frame(width: 300, height: 300)
+                
+                Spacer()
+                
+                Button(action: {
+                
+                }, label: {
+                    Text("New Image")
+                        .bold()
+                        .frame(width: 250, height: 65)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                        .padding()
+                })
+            }
+            .navigationTitle("Photo Mania")
+        }
     }
 }
 
